d618792a4ff81bc4cd2d4e1eb41ba13310ed8aba
rayvo50 <raviregalo@gmail.com>
2022-03-14 18:43:28 +0000

    added nothing

--- ring.c
+++ ring.c
@@ -6,13 +6,12 @@
 #include <string.h>
 
 int BUFFER_SIZE = 256;
+//char** command_list = ["new", "bentry", "pentry", "chord", "echord", "show", "find", "leave"];
 
 int main(int argc, char** argv)
 {
     int node_i, node_port;
     char command[BUFFER_SIZE], *node_ip;
-    
-    printf("check");
 
     if(argc != 4){
         printf("Usage: ring i i.ip i.port");
@@ -23,12 +22,42 @@ int main(int argc, char** argv)
         node_ip = argv[2];
         node_port = atoi(argv[3]);
     }
-
-    printf("check");
+    
+    
 
     while(1){
-        fgets(command, BUFFER_SIZE-1, stdin);
-        printf("%s", command);
+        fgets(command, BUFFER_SIZE, stdin);
+        
+        switch (command[0])
+        {
+        case 'n':
+            new();            
+            break;
+        case 'b':
+            bentry();
+            break;
+        case 'p':
+            pentry();
+            break;
+        case 'c':
+            chord();
+            break;
+        case 'e':
+            echord();
+            break;
+        case 's':
+            show();
+            break;
+        case 'f':
+            find();
+            break;
+        case 'l':
+            leave();
+            break;
+        default:
+        exit(1);
+            break;
+        }
     }
 
 
